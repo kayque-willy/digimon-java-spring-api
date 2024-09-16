7525cf314b1dd53fdc52da428074aa49a9e5f313
Kayque <kayque-willy@hotmail.com>
2024-09-16 18:07:50 -0300

    Adição da imagem do Docker

--- digimon/src/main/java/com/api/digimon/DigimonApplication.java
+++ digimon/src/main/java/com/api/digimon/DigimonApplication.java
@@ -2,13 +2,20 @@ package com.api.digimon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DigimonApplication {
+public class DigimonApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DigimonApplication.class, args);
 		System.out.println("API listen on port 8080: http://localhost:8080/api/digimons");
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DigimonApplication.class);
+    }
 
 }
